31826e58644002803d5c9f62339131d334a50495
Jissmon Jose <jissmon476@gmail.com>
2024-07-14 16:09:26 +0530

    commit all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <h1>Welcome to QTify</h1>
         <p>Your music streaming application.</p>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar goes here
+    </div>
+  )
+}
+
+export default Navbar
